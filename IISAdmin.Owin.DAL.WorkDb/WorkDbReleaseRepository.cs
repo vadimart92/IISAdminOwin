92ce58e51aba811af96176982f30aa50e3f1573e
Vadym <vadimart92@gmail.com>
2015-02-26 22:15:30 +0200

    todo

--- IISAdmin.Owin.DAL.WorkDb/WorkDbReleaseRepository.cs
+++ IISAdmin.Owin.DAL.WorkDb/WorkDbReleaseRepository.cs
@@ -17,10 +17,12 @@ namespace IISAdmin.Owin.DAL.Common {
 
 	public class WorkDbReleaseRepository : IReleaseRepository {
 		public void Create(IRelease entity) {
+			//todo IISAdmin.Owin.DAL.Common.WorkDbReleaseRepository
 			throw new NotImplementedException();
 		}
 
 		public IRelease Get(Guid key) {
+			//todo IISAdmin.Owin.DAL.Common.WorkDbReleaseRepository
 			return new WorkDbRelease {
 				Id = key,
 				BuildFolderLink = @"\\tscrm.com\dfs-ts\Builds-7\7.4.1\7.4.1.702\SalesOmnichannel_Softkey_RUS\7.4.1.702_SalesOmnichannel_Softkey_MSSQL_RUS.zip",
@@ -32,6 +34,7 @@ namespace IISAdmin.Owin.DAL.Common {
 		}
 
 		public IEnumerable<IRelease> GetAll() {
+			//todo IISAdmin.Owin.DAL.Common.WorkDbReleaseRepository
 			var res = new List<IRelease>();
 			for (int i = 0; i < 10; i++) {
 				res.Add(new WorkDbRelease {
@@ -44,14 +47,15 @@ namespace IISAdmin.Owin.DAL.Common {
 
 		public IEnumerable<IRelease> GetAll(Expression<Func<IRelease, bool>> expression) {
 			throw new NotImplementedException();
+			//todo IISAdmin.Owin.DAL.Common.WorkDbReleaseRepository
 		}
 
 		public void Update(IRelease entity) {
-			throw new NotImplementedException();
+			throw new InvalidOperationException();
 		}
 
 		public void Delete(Guid id) {
-			throw new NotImplementedException();
+			throw new InvalidOperationException();
 		}
 
 		public IRelease GetByUri(string uri) {
